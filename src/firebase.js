51fe5a3000b627db142e4536d09fb67a791c78ee
Richard <richardrose060878@gmail.com>
2021-02-03 22:11:20 +0000

    Frontend of the build

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDBy2kI4WLVGpz6zIgG-w2rJ0oevxsxPpk',
+  authDomain: 'netflix-build-93520.firebaseapp.com',
+  projectId: 'netflix-build-93520',
+  storageBucket: 'netflix-build-93520.appspot.com',
+  messagingSenderId: '917437524568',
+  appId: '1:917437524568:web:9bf49ceea868a29e67b82d',
+}
