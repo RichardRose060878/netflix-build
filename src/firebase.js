51e2a8702a38413dc5fb67a40177020fd4c04c52
Richard <richardrose060878@gmail.com>
2021-02-04 22:21:40 +0000

    login and authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: 'AIzaSyDBy2kI4WLVGpz6zIgG-w2rJ0oevxsxPpk',
   authDomain: 'netflix-build-93520.firebaseapp.com',
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: '917437524568',
   appId: '1:917437524568:web:9bf49ceea868a29e67b82d',
 }
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth }
+export default db
